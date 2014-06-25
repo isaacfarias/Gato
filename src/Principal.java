335117e14cfe7811dea300f8b1616ea932313148
np305Windows <men_one89@hotmail.com>
2014-06-24 23:00:28 -0500

    Ready the Gui, but that can change in the future!

--- src/Principal.java
+++ src/Principal.java
@@ -2,12 +2,23 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+
 import java.awt.BorderLayout;
+
+import javax.swing.JSplitPane;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 public class Principal {
 
 	private JFrame frame;
+	private JButton tablero[][];
 
 	/**
 	 * Launch the application.
@@ -40,8 +51,49 @@ public class Principal {
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JLabel lblEsteSeraUn = new JLabel("Este sera un juego de Gato");
-		frame.getContentPane().add(lblEsteSeraUn, BorderLayout.CENTER);
+		JSplitPane splitPane = new JSplitPane();
+		splitPane.setOrientation(JSplitPane.VERTICAL_SPLIT);
+		frame.getContentPane().add(splitPane, BorderLayout.CENTER);
+		
+		JPanel panel = new JPanel();
+		splitPane.setRightComponent(panel);
+		panel.setLayout(new GridLayout(3, 3, 0, 0));
+		
+		JPanel panel_1 = new JPanel();
+		splitPane.setLeftComponent(panel_1);
+		
+		JButton btnReset = new JButton("reset");
+		btnReset.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				reset();
+			}
+		});
+		panel_1.add(btnReset);
+		tablero = new JButton[3][3];
+		for (int i = 0; i<3; i++)
+		{
+			for(int j = 0;j<3;j++)
+			{
+				tablero[i][j] = new JButton("");
+				panel.add(tablero[i][j]);
+				tablero[i][j].setActionCommand(""+i+" "+j);
+				tablero[i][j].addActionListener(new ActionListener(){
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						String[] spl_ret = e.getActionCommand().split(" ");
+						int a = Integer.parseInt(spl_ret[0]);
+						int b = Integer.parseInt(spl_ret[1]);
+					}
+					
+				}
+				);
+			}
+		}
+	}
+	
+	private void reset()
+	{
+		System.out.println("Reset method");
 	}
 
 }
